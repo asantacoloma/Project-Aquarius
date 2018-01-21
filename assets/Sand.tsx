7e64d3a60df78150b530d52ae289b16de1eac07a
asantacoloma24 <asantacoloma24@yahoo.com>
2018-01-21 06:56:14 -0500

    FIrst commit to repository

---
+++ assets/Sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Sand" tilewidth="64" tileheight="64" tilecount="30" columns="5">
+ <image source="sand.png" width="320" height="384"/>
+</tileset>
---
+++ assets/tilemaps/tiles/sand2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sand2" tilewidth="64" tileheight="64" tilecount="30" columns="5">
+ <image source="sand2.png" width="320" height="384"/>
+</tileset>
